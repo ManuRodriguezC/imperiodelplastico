fcba596154dcf3e05d4456107d42dc363e499537
ManuRodriguezC <estebanclimb@gmail.com>
2025-04-06 12:05:58 -0500

    check astro dependences for deploy in vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
   output: 'server',
-  // adapter: vercel({
-  //   webAnalytics: {
-  //     enabled: true
-  //   }
-  // }),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
   integrations: [react()]
 });
