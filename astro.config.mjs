b9acdf1fdc579cf26ca8094f462ca309355fe9f5
ManuRodriguezC <estebanclimb@gmail.com>
2025-04-06 21:10:04 -0500

    add Nav movil

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,6 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  }),
+  adapter: vercel(),
   integrations: [react()]
 });
--- src/components/tsx/Contacto.tsx
+++ src/components/tsx/Contacto.tsx
@@ -1,12 +1,17 @@
 import type React from "react";
+import { useState } from "react";
 
 export default function FormContact() {
 
+  const [controlSend, setControlSend] = useState(false)
+  const [loading, setLoading] = useState(false)
+
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
+    const form = event.currentTarget;
     const formData = new FormData(event.currentTarget)
     const data = Object.fromEntries(formData.entries())
+    setLoading(true)
     try {
       const response = await fetch('/api/sendEmail', {
         method: 'POST',
@@ -16,17 +21,18 @@ export default function FormContact() {
 
       const result = await response.json()
       if (result.message === "Correo Enviado") {
-        console.log("Se ha enviado")
+        setControlSend(true)
+        form.reset()
       }
     } catch (e) {
       console.log("No se ha enviado el correo")
     } finally {
-      console.log("Finalizo")
+      setLoading(false)
     }
   }
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-5 rounded-xl shadow-2xl w-[500px]">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-5 rounded-xl shadow-2xl w-auto lg:w-[500px]">
       <p className="text-xl color-imperio font-semibold border-b-[1px] border-red-500">
         Déjanos tus datos de contacto
       </p>
@@ -46,8 +52,17 @@ export default function FormContact() {
         <label className="text-gray-800 text-sm" htmlFor="mensaje">Mensaje</label>
         <textarea id="mensaje" name="mensaje" className="border-[1px] border-gray-200 rounded-md p-2" required />
       </div>
-      <button type="submit" className="bg-imperio rounded-md text-white cursor-pointer mt-4 p-2">
-        Enviar
+      {
+        controlSend &&
+        <p className="text-center">Hemos recibido tu correo. Muy pronto nos pondremos en contacto contigo.</p>
+      }
+      <button
+        type="submit"
+        disabled={loading}
+        className="bg-imperio rounded-md text-white cursor-pointer mt-4 p-2">
+        {
+          loading ? "Enviando" : "Enviar"
+        }
       </button>
     </form>
   );
