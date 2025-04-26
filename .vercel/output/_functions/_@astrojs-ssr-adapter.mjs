b09a99360cf24a7ad96c309d5be03a3449f62018
ManuRodriguezC <estebanclimb@gmail.com>
2025-04-26 10:53:56 -0500

    Delete vercel

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_CP9kg8MG.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BqLvf2SL.mjs';
-export { renderers } from '../renderers.mjs';
